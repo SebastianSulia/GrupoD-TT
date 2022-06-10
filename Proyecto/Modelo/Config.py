3fc150f445d083db9d32d48d6d60ea4d441b5aad
SebastianSulia <angel.sulia@alumnos.frm.utn.edu.ar>
2022-06-10 19:04:28 -0300

    Creo archivo de config para guardar datos

--- Proyecto/Modelo/Config.py
+++ Proyecto/Modelo/Config.py
@@ -8,9 +8,34 @@ class Config:
         self.precio_final_full: float = 0.0
 
     def set_valores_iniciales(self):
-        self.costoTinta = 1650
-        self.costoPapel = 650
-        self.servicioTecnico = 8000
-        self.ganancia = 3.3
-        self.costo_final_full = ((self.costoTinta*4)+(self.costoPapel*2)+self.servicioTecnico)/1000
+        config = get_config()
+        self.costoTinta = float(config[0])
+        self.costoPapel = float(config[1])
+        self.servicioTecnico = float(config[2])
+        self.ganancia = float(config[3])
+        self.costo_final_full = ((self.costoTinta * 4) + (self.costoPapel * 2) + self.servicioTecnico) / 1000
         self.precio_final_full = self.costo_final_full * self.ganancia
+
+
+def set_valores_reset():
+    valor_inicial= '1;1;1;1'
+    guardar_config(valor_inicial)
+
+
+
+def guardar_config(datos):
+    fichero = open(
+        r"/Users/sebastiansulia/Documents/Tecnicatura_programacion/Semestre2/Metodología/Proyecto/Repos/GrupoD-TT/Proyecto/Historial/config.txt",
+        'w')
+    fichero.write(datos)
+    fichero.close()
+
+
+def get_config():
+    f = open(
+        r"/Users/sebastiansulia/Documents/Tecnicatura_programacion/Semestre2/Metodología/Proyecto/Repos/GrupoD-TT/Proyecto/Historial/config.txt",
+        'r')
+    text = f.read()
+    f.close()
+    config = text.split(';')
+    return config
