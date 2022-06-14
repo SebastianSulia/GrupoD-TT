c2f23fea76cd6871f979143059f36ade422847bc
emiliachiofalo <maria.chiofalo@alumnos.frm.utn.edu.ar>
2022-06-13 22:33:39 -0300

    Ingreso imagen para analisis y calculo cobertura A4 y A5

--- Proyecto/Modelo/PaginaService.py
+++ Proyecto/Modelo/PaginaService.py
@@ -1,6 +1,7 @@
 import cv2
 import os.path
 from PIL import Image
+from Modelo.Imagen import *
 
 
 def analisis_pixel_BGR(path):
@@ -21,3 +22,60 @@ def analisis_pixel_BGR(path):
 
         datospagina = [imgg.width, imgg.height, coberturacolor]
         return datospagina
+
+def analisis_pixel_BGR_Imagen(imagen, path):
+    with Image.open(path) as imgg:
+
+        im1 = imgg
+        #impresionA4
+        if im1.width>im1.height:
+            im1.thumbnail((842, 595))
+            im1.save(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+        else:
+            im1.thumbnail((595, 842))
+            im1.save(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+
+        cantPixA4 = 842*595
+        cantPixSobra = cantPixA4 - (im1.height*im1.width)
+        porcentajePixBlancosHj = cantPixSobra*100/cantPixA4
+
+        im = cv2.imread(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+
+        #analisis pixel
+        contB = 0
+        for fila in range(im1.height):
+            for col in range(im1.width):
+                if im[fila, col][0] >= 230 and im[fila, col][1] >= 230 and im[fila, col][2] >= 230:
+                    contB = contB + 1
+
+        cont2 = contB / (im1.width * im1.height)
+
+        coberturabco = cont2 * 100
+        imagen.coberturaA4 = 100 - (coberturabco + porcentajePixBlancosHj)
+
+        im2 = imgg
+        #impresionA5
+        if im2.height>im2.width:
+            im2.thumbnail((420, 595))
+            im2.save(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+        else:
+            im2.thumbnail((595, 420))
+            im2.save(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+
+        cantPixA4 = 842*595
+        cantPixSobra = cantPixA4 - (im2.height*im2.width)
+        porcentajePixBlancosHj2 = cantPixSobra*100/cantPixA4
+
+        im = cv2.imread(os.path.dirname(_file_).replace("Modelo", "Temp") + "/Img.jpeg")
+
+        #analisis pixel
+        contB = 0
+        for fila in range(im2.height):
+            for col in range(im2.width):
+                if im[fila, col][0] >= 230 and im[fila, col][1] >= 230 and im[fila, col][2] >= 230:
+                    contB = contB + 1
+
+        cont2 = contB / (im2.width * im2.height)
+
+        coberturabco2 = cont2 * 100
+        imagen.coberturaA5 = 100 - (coberturabco2 + porcentajePixBlancosHj2)
